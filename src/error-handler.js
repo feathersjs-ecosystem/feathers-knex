a096456afef0cd77113e8e4eb3fd1b4841b99fdc
lvivier <luke@vivier.ca>
2017-02-10 14:49:23 -0800

    Add error handling for mysql and mysql2 server errors

--- src/error-handler.js
+++ src/error-handler.js
@@ -3,7 +3,42 @@ import errors from 'feathers-errors';
 export default function errorHandler (error) {
   let feathersError = error;
 
-  // TODO (EK): Map PG, MySQL, Oracle, etc. errors
+  // TODO (EK): Map PG, Oracle, etc. errors
+
+  // NOTE: SQLState values from
+  // https://dev.mysql.com/doc/connector-j/5.1/en/connector-j-reference-error-sqlstates.html
+
+  if (error.sqlState && error.sqlState.length) {
+    // remove SQLSTATE marker (#) and pad/truncate SQLSTATE to 5 chars
+    let sqlState = ('00000' + error.sqlState.replace('#', '')).slice(-5);
+
+    switch (sqlState.slice(0, 2)) {
+      case '02':
+        feathersError = new errors.NotFound(error);
+        break;
+      case '28':
+        feathersError = new errors.Forbidden(error);
+        break;
+      case '08':
+      case '0A':
+      case '0K':
+        feathersError = new errors.Unavailable(error);
+        break;
+      case '20':
+      case '21':
+      case '22':
+      case '23':
+      case '24':
+      case '25':
+      case '40':
+      case '42':
+      case '70':
+        feathersError = new errors.BadRequestError(error);
+        break;
+      default:
+        feathersError = new errors.GeneralError(error);
+    }
+  }
 
   // NOTE (EK): Error codes taken from
   // https://www.sqlite.org/c3ref/c_abort.html
