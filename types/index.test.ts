9768f226130e6489fafec7f73b2060f2709b4660
David Luecke <daff@neyeon.com>
2019-07-05 18:21:55 -0700

    Add TypeScript definitions and upgrade tests to Feathers 4 (#211)

---
+++ types/index.test.ts
@@ -0,0 +1,27 @@
+import { default as knexService, hooks } from 'feathers-knex';
+import feathers from '@feathersjs/feathers';
+import * as knex from 'knex';
+
+const db = knex({
+  client: 'sqlite3',
+  connection: {
+    filename: './db.sqlite'
+  }
+});
+
+const service = knexService({
+  Model: db,
+  name: 'test'
+});
+
+const app = feathers().use('/test', service);
+
+app.service('test').hooks({
+  before: {
+    all: [
+      hooks.transaction.start(),
+      hooks.transaction.end(),
+      hooks.transaction.rollback()
+    ]
+  }
+});
